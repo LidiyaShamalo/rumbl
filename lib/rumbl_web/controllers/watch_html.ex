588335827a4ff9e9ce7c9a058460f041b5316b07
LidiyaShamalo <arkteya07@gmail.com>
2026-03-29 18:40:22 +0300

    creating a layout for video viewing

---
+++ lib/rumbl_web/controllers/watch_html.ex
@@ -0,0 +1,10 @@
+defmodule RumblWeb.WatchHTML do
+  use RumblWeb, :html
+
+  def player_id(video) do
+    ~r{^.*(?:youtu\.be/|\w+/|v=)(?<id>[^#&?]*)}
+    |> Regex.named_captures(video.url)
+    |> get_in(["id"])
+  end
+
+end
