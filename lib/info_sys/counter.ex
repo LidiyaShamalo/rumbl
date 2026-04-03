66be13f90b598661cfa29d0e63e23c7aa12d2cbb
LidiyaShamalo <arkteya07@gmail.com>
2026-04-03 17:01:40 +0300

    using GenServer

--- lib/info_sys/counter.ex
+++ lib/info_sys/counter.ex
@@ -1,45 +1,31 @@
 defmodule InfoSys.Counter do
+  use GenServer
 
-  def child_spec(opts) do
-    %{
-      id: __MODULE__,
-      start: {__MODULE__, :start_link, [opts]},
-      type: :worker,
-      restart: :permanent,
-      shutdown: 500
-    }
-  end
-  
-  def inc(pid), do: send(pid, :inc)
+  def inc(pid), do: GenServer.cast(pid, :inc)
 
-  def dec(pid), do: send(pid, :dec)
+  def dec(pid), do: GenServer.cast(pid, :dec)
 
-  def val(pid, timeout \\ 5000) do
-    ref = make_ref()
-    send(pid, {:val, self(), ref})
-
-    receive do
-      {^ref, val} -> val
-    after
-      timeout -> exit(:timeout)
-    end
+  def val(pid) do
+    GenServer.call(pid, :val)
   end
 
   def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
+    GenServer.start_link(__MODULE__, initial_val)
   end
 
-  defp listen(val) do
-    receive do
-      :inc ->
-        listen(val + 1)
+  def init(initial_val) do
+    {:ok, initial_val}
+  end
 
-      :dec ->
-        listen(val - 1)
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
 
-      {:val, sender, ref} ->
-        send(sender, {ref, val})
-        listen(val)
-    end
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
   end
 end
